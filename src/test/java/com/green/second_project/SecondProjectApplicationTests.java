081b7b85c285b70a277c31b5493ea85461691579
jangjinseong <hn04223@gmail.com>
2025-01-15 15:46:26 +0900

    readme dd

---
+++ src/test/java/com/green/second_project/SecondProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.second_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
